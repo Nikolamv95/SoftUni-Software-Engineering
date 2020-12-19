22a6be3c6146f3696e37ddb1070c3acc6440e9c8
Nikolamv95 <nikola.margaritoov@gmail.com>
2020-12-19 10:24:00 +0200

    Adding Tasks from Fundamentals course

---
+++ CSharp-Technology-Fundamentals/1.BasicSuntax,ConditionalStatementsAndLoops/1.Lecture/SumOfOddNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var sum = 0;
+            var loops = 1;
+            var oddOrEven = 1;
+
+
+            while (loops <= number)
+            {
+                if (oddOrEven % 2 == 1)
+                {
+                    Console.WriteLine(oddOrEven);
+                    sum += oddOrEven;
+                    oddOrEven += 2;
+                }
+                loops += 1;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+
+        } 
+    }
+}
---
+++ CSharp-Technology-Fundamentals/1.BasicSuntax,ConditionalStatementsAndLoops/1.Lecture/Tasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} {1}", name, age);
+
+
+            double number = 5.50003214;
+            int number2 = 55;
+
+            Console.WriteLine("{0:f2}",number);
+            Console.WriteLine("{0:D3}",number2);
+
+                
+
+        }
+    }
+}
