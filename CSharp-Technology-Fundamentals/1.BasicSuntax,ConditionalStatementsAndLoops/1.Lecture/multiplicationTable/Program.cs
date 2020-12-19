0e061b64fe50dfccd6e64d1013fda3a59c6d2720
Nikolamv95 <nikola.margaritoov@gmail.com>
2020-12-19 10:25:59 +0200

    Adding tasks in fundamentals course

---
+++ CSharp-Technology-Fundamentals/1.BasicSuntax,ConditionalStatementsAndLoops/1.Lecture/multiplicationTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var result = 0;
+
+            for (int i = number; i == number; i++)
+            {
+                for (int times = 1; times <= 10; times++)
+                {
+                    result = number * times;
+
+                    Console.WriteLine($"{number} X {times} = {result}");
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Technology-Fundamentals/1.BasicSuntax,ConditionalStatementsAndLoops/1.Lecture/multiplicationTable2.0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace multiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            var times = int.Parse(Console.ReadLine());
+            var result = 0;
+
+            for (int i = number; i == number; i++)
+            {
+                if (times > 10)
+                {
+                    for (int n = times; n == times; n++)
+                    {
+                        result = number * times;
+
+                        Console.WriteLine($"{number} X {times} = {result}");
+                    } 
+                }
+                else
+                {
+                    for (int z = times; z <= 10; z++)
+                    {
+                        result = number * times;
+                        Console.WriteLine($"{number} X {times} = {result}");
+                        times += 1;
+                    }
+                }
+            }
+        }
+    }
+}
