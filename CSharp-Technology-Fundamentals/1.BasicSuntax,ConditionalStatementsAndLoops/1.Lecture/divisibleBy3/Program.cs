bee090280f631f64d129185695376b12c46a6d07
Nikolamv95 <nikola.margaritoov@gmail.com>
2020-12-20 20:10:31 +0200

    Adding tasks to Fudnamentals.

---
+++ CSharp-Technology-Fundamentals/1.BasicSuntax,ConditionalStatementsAndLoops/1.Lecture/divisibleBy3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace divisibleBy3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+
+            for (int i = 3; i < number; i+=3)
+            {
+                    Console.WriteLine(i);
+
+            }
+        }
+    }
+}
---
+++ CSharp-Technology-Fundamentals/1.BasicSuntax,ConditionalStatementsAndLoops/1.Lecture/foreignLanguages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace foreignLanguages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string language = Console.ReadLine();
+
+            if (language == "England" || language == "USA")
+            {
+                Console.WriteLine("English");
+            }
+            else if (language == "Spain" || language == "Argentina" || language == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
