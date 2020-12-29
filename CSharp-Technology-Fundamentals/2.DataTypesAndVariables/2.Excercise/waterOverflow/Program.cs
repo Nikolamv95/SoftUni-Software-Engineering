f08f0f0af98dbaf7b67f074bc270eecddc6a97bf
Nikolamv95 <nikola.margaritoov@gmail.com>
2020-12-29 15:40:24 +0200

    Adding Tasks to fundamentals

---
+++ CSharp-Technology-Fundamentals/2.DataTypesAndVariables/2.Excercise/waterOverflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace waterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+            int waterCapacity = 255;
+
+            while (nLines > 0)
+            {
+                int waterToAdd = int.Parse(Console.ReadLine());
+
+                if (waterCapacity > waterToAdd)
+                {
+                    waterCapacity -= waterToAdd;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(waterCapacity);
+        }
+    }
+}
