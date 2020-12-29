08dbff90bd91b459816e652c9ebffe6b4e60d13e
Nikolamv95 <nikola.margaritoov@gmail.com>
2020-12-29 15:41:54 +0200

    Adding tasks to fundamentals

---
+++ CSharp-Technology-Fundamentals/2.DataTypesAndVariables/3.DoubleCheck/specialNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace specialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int numberDevide = number;
+            int lastNum = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                numberDevide = i;
+
+                for (int j = 0; j <= numberDevide; j++)
+                {
+                    lastNum = numberDevide % 10;
+                    sum += lastNum;
+                    numberDevide /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+
+                sum = 0;
+                
+            }
+        }
+    }
+}
---
+++ CSharp-Technology-Fundamentals/2.DataTypesAndVariables/3.DoubleCheck/waterOverflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace waterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            int capacity = 255;
+            int sum = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                int quantities = int.Parse(Console.ReadLine());
+
+                if (capacity >= quantities)
+                {
+                    capacity -= quantities;
+                    sum += quantities;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
