12c9c04b1ff929a28f9716824e6360b20e4b5377
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-01-06 20:27:02 +0200

    adding tasks to fundamentals

---
+++ CSharp-Technology-Fundamentals/2.DataTypesAndVariables/3.DoubleCheck/pokeMon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int powerN = int.Parse(Console.ReadLine());
+            int distanceM = int.Parse(Console.ReadLine());
+            int factoryY = int.Parse(Console.ReadLine());
+            int hit = 0;
+            double devide50 = (double)powerN / 2;
+
+            while (powerN >= distanceM)
+            {
+                powerN -= distanceM;
+
+                if (powerN == devide50 && powerN != 0 && factoryY != 0)
+                {
+                    powerN /= factoryY;
+                }
+
+                hit += 1;
+            }
+
+            Console.WriteLine(powerN);
+            Console.WriteLine(hit);
+        }
+    }
+}
