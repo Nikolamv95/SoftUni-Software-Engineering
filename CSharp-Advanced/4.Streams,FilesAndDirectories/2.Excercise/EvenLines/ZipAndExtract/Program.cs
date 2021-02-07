5574d96b1718bbadbcf6d7907115dc3dddcf88e7
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-07 10:38:46 +0200

    Adding tasks to Advanced

---
+++ CSharp-Advanced/4.Streams,FilesAndDirectories/2.Excercise/EvenLines/ZipAndExtract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Зипваме цялата папка
+            //string path = "./";
+            //string dest = "./../myzip.zip";
+            //ZipFile.CreateFromDirectory(path, dest);
+
+            string filePath = "../../../words.txt";
+            using ZipArchive zipArchive = ZipFile.Open("../../../arhiv.zip", ZipArchiveMode.Create);
+            zipArchive.CreateEntryFromFile(filePath, "newName.txt");
+        }
+    }
+}
