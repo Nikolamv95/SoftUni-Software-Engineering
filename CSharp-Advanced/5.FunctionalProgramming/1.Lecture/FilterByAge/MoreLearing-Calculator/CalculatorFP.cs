b6b0061b16176dfcc9abfe575a7d99e01fdc3d2e
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-08 08:23:26 +0200

    Update C# Advanced

---
+++ CSharp-Advanced/5.FunctionalProgramming/1.Lecture/FilterByAge/MoreLearing-Calculator/CalculatorFP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreLearing_Calculator
+{
+    class CalculatorFP
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Miltiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Devide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Minus(int a, int b)
+        {
+            return a / b;
+        }
+
+        public void PrintResult(int value)
+        {
+            Console.WriteLine($"The result is: {value}");
+        }
+    }
+}
---
+++ CSharp-Advanced/5.FunctionalProgramming/1.Lecture/FilterByAge/MoreLearing-Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoreLearing_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            var calculator = new CalculatorFP();
+            
+            switch (operation)
+            {
+                case "sum":
+                    calculator.PrintResult(calculator.Sum(a, b));
+                    break;
+                case "minus":
+                    calculator.PrintResult(calculator.Minus(a, b));
+                    break;
+                case "multiply":
+                    calculator.PrintResult(calculator.Miltiply(a, b));
+                    break;
+                case "devide":
+                    calculator.PrintResult(calculator.Devide(a, b));
+                    break;
+            }
+        }
+    }
+}
