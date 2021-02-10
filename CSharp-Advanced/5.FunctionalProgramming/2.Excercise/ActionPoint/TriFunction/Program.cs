b0a11deb83e2ab94f44b4b446eac3c5e6e95a58a
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-10 09:07:02 +0200

    Adding tasks to C# Advanced

---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/TriFunction/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<string> listOfPeople = Console.ReadLine().Split().ToList();
+            Func<string, int> getAsciiSum = p => p.Select(c =>(int)c).Sum();
+            Func<List<string>, int, Func<string, int>, string> nameFunc = (listOfPeople, number, getAsciiSum)
+                 => listOfPeople.FirstOrDefault(p => getAsciiSum(p) >= number);
+            string result = nameFunc(listOfPeople, number, getAsciiSum);
+            Console.WriteLine(result);
+
+
+            string person = GetName(listOfPeople, number, getAsciiSum);
+            Console.WriteLine(person);
+
+        }
+
+        static string GetName(List<string> people, int number, Func<string, int> func)
+        {
+            string res = null;
+
+            foreach (var item in people)
+            {
+                if (func(item) >= number)
+                {
+                    res = item;
+                    break;
+                }
+            }
+
+            return res;
+        }
+    }
+}
