11084ddad4b0905c7564b0a8adfedf7a3dbc46c7
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-11 08:10:22 +0200

    Adding tasks to C# Advanced

---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/CustomMinFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> myParse = x => int.Parse(x);
+            int[] numsInput = Console.ReadLine().Split(' ').Select(myParse).ToArray();
+
+            Func<int[], int> smallestNums = CheckSmallestNum(numsInput);
+            Console.WriteLine(smallestNums(numsInput)); 
+        }
+
+        static Func<int[], int> CheckSmallestNum(int[] num)
+        {
+            int smallestNum = int.MaxValue;
+
+            foreach (var item in num)
+            {
+                if (item < smallestNum)
+                {
+                    smallestNum = item;
+                }
+            }
+
+            return nums => smallestNum;
+        }
+    }
+}
---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/FindEvensOrOdds/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> myParse = x => int.Parse(x);
+            int[] nums = Console.ReadLine().Split().Select(myParse).ToArray();
+            string oddOrEven = Console.ReadLine();
+            int currNum = 0;
+            Func<int, bool> numChecker = VerifyOddOrEven(currNum, oddOrEven);
+
+            for (int i = nums[0]; i <= nums[1]; i++)
+            {
+                currNum = i;
+                if (numChecker(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+
+        private static Func<int, bool> VerifyOddOrEven(int currNum, string oddOrEven)
+        {
+            switch (oddOrEven)
+            {
+                case "odd": return p => p % 2 != 0;
+                case "even": return p => p % 2 == 0;
+                default: return null;
+            }
+        }
+    }
+}
