1bc06492ddb35214b2e5ea805e6390d768d5abbd
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-11 08:10:05 +0200

    Adding tasks to C# Advanced

---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/FindEvensOrOdds-Predicate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace FindEvensOrOdds_Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> myParse = x => int.Parse(x);
+            int[] nums = Console.ReadLine().Split().Select(myParse).ToArray();
+            string oddOrEven = Console.ReadLine();
+            int currNum = 0;
+            Predicate<int> oddOrEvenPredicate = FindOddOrEven(currNum, oddOrEven);
+
+            for (int i = nums[0]; i <= nums[1]; i++)
+            {
+                currNum = i;
+
+                if (oddOrEvenPredicate(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+
+        static Predicate<int> FindOddOrEven(int currNum, string oddOrEven)
+        {
+            switch (oddOrEven)
+            {
+                case "odd": return p => p % 2 != 0;
+                case "even": return p => p % 2 == 0;
+                default: return null;
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/KnightsOfHonor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namesInput = Console.ReadLine().Split(' ');
+            Action<string> printData = name =>
+            {
+                Console.WriteLine($"Sir {name}");
+            };
+
+            PrintCollection(namesInput, printData);
+        }
+
+        private static void PrintCollection(string[] namesInput, Action<string> printData)
+        {
+            foreach (var item in namesInput)
+            {
+                printData(item);
+            }
+        }
+    }
+}
