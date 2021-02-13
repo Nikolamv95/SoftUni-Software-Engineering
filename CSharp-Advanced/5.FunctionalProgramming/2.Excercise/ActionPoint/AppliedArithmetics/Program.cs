e1372867ccae44f7150841c19f1fc63ff31177f4
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-13 11:32:02 +0200

    Adding tasks to C# Advanced

---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/AppliedArithmetics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> customSubtract = x => x - 1;
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        numbers = numbers.Select(x => x + 1).ToArray();
+                        break;
+                    case "multiply":
+                        numbers = numbers.Select(x => x * 2).ToArray();
+                        break;
+                    case "subtract":
+                        numbers = numbers.Select(customSubtract).ToArray();
+                        break;
+                    case "print":
+                        Console.WriteLine(string.Join(' ', numbers));
+                        break;
+                }
+            }
+        }
+    }
+}
