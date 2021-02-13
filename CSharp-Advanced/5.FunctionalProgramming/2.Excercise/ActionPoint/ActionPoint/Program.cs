1a3e59d1b9f80cbfba3c5b46bcce40264a7cf153
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-13 11:32:21 +0200

    Adding tasks to C# Advanced

---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/ActionPoint/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] namesInput = Console.ReadLine().Split(' ');
+            Action<string> printData = PrintData(namesInput);
+            PrintCollection(namesInput, printData);
+        }
+
+        private static void PrintCollection(string[] namesInput, Action<string> printData)
+        {
+            foreach (var item in namesInput)
+            {
+                printData(item);
+            }
+        }
+
+        static Action<string> PrintData(string [] namesInput)
+        {
+            return name =>
+            {
+                Console.WriteLine($"{name}");
+            };
+        }
+    }
+}
