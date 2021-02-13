21cd1adcb901a2d8f8d028844131b07f183d81e2
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-13 11:31:46 +0200

    Adding tasks to C# Advanced

---
+++ CSharp-Advanced/5.FunctionalProgramming/2.Excercise/ActionPoint/CustomComparator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(numbers, (x, y) =>
+            (x % 2 == 0 && y % 2 != 0) ? -1 :
+            (x % 2 != 0 && y % 2 == 0) ? 1 :
+            x.CompareTo(y));
+
+            //Или
+            //{
+            //    int sorter = 0;
+            //    if (x % 2 == 0 && y % 2 != 0)
+            //    {
+            //        sorter = -1;
+            //    }
+            //    else if (x % 2 != 0 && y % 2 == 0)
+            //    {
+            //        sorter = 1;
+            //    }
+            //    else
+            //    {
+            //        sorter = x.CompareTo(y);
+            //    }
+            //    return sorter;
+            //});
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
