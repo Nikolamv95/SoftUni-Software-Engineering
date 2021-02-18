ebbfc5d4de9a20c75f7236bbfbf8ee88d12d7d32
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-18 22:23:21 +0200

    Adding tasks to C# Advanced

---
+++ CSharp-Advanced/6.DefiningClasses/1.Lecture/CarExtension/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            {
+                double expenceFuel = FuelConsumption * distance / 100;
+
+                if (expenceFuel > FuelQuantity)
+                {
+                    Console.WriteLine("Not enough fuel to perform this trip!");
+                }
+                else
+                {
+                    FuelQuantity -= distance / 100 * FuelConsumption;
+                }
+            }
+        }
+
+        public string WhoAmI()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString();
+        }
+    }
+}
