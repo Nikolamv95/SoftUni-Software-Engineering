6ae2bdebb539b3c77a6514f5c6a2a8e9c3c8c6f6
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-21 11:19:53 +0200

    C# Advanced tasks

---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/PokemonTrainer/StartUp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            List<Trainer> listOfTrainers = new List<Trainer>();
+            
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] data = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string nameTrainer = data[0];
+                string namePokemon = data[1];
+                string elementPokemon = data[2];
+                int healthPokemon= int.Parse(data[3]);
+                int numOfBadges = 0;
+
+                Pokemon currPokemon = new Pokemon(namePokemon, elementPokemon, healthPokemon);
+                
+
+                if (listOfTrainers.Exists(x=>x.Name == nameTrainer) == false)
+                {
+                    List<Pokemon> listOfPokemons = new List<Pokemon>();
+                    listOfPokemons.Add(currPokemon);
+                    Trainer currTrainer = new Trainer(nameTrainer, numOfBadges, listOfPokemons);
+                    listOfTrainers.Add(currTrainer);
+                }
+                else
+                {
+                    var tu = listOfTrainers.Single(x => x.Name == nameTrainer);
+                    tu.Pokemons.Add(currPokemon);
+                }
+            }
+
+            string badge = string.Empty;
+
+            while ((badge = Console.ReadLine()) != "End")
+            {
+                for (int i = 0; i < listOfTrainers.Count; i++)
+                {
+                    string nameTrainer = listOfTrainers[i].Name;
+
+                    if (listOfTrainers[i].Pokemons.Exists(x => x.Element == badge))
+                    {
+                        listOfTrainers[i].NumOfBadges++;
+                    }
+                    else
+                    {
+                        var tu = listOfTrainers.Single(x => x.Name == nameTrainer);
+
+                        for (int z = 0; z < listOfTrainers[i].Pokemons.Count; z++)
+                        {
+                            listOfTrainers[i].Pokemons[z].Health -= 10;
+                            bool isDied = listOfTrainers[i].Pokemons.Exists(x => x.Health <= 0);
+
+                            if (isDied)
+                            {
+                                tu.Pokemons.RemoveAll(x => x.Health < 0);
+                                z--;
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var item in listOfTrainers.OrderByDescending(x=>x.NumOfBadges))
+            {
+                Console.WriteLine($"{item.Name} {item.NumOfBadges} {item.Pokemons.Count}");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/PokemonTrainer/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonTrainer
+{
+    class Trainer
+    {
+        public Trainer(string name, int numOfBadges, List<Pokemon> pokemons)
+        {
+            this.Name = name;
+            this.NumOfBadges = numOfBadges;
+            this.Pokemons = pokemons;
+        }
+
+        public string Name { get; set; }
+        public int NumOfBadges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+    }
+}
