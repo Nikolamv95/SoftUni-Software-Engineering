8085df531386e370a07a0f5c474b9c39d6ce769c
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-21 11:19:39 +0200

    C# Advanced tasks

---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/RawData/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.Tires = tires;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public void PrintFragile(List<Car> listOfCars)
+        {
+            var filtList = listOfCars.Where(x => x.Cargo.CargoType == "fragile");
+
+            foreach (var item in listOfCars)
+            {
+                double sumTire = item.Tires.Sum(x => x.TirePressure);
+
+                if (sumTire / 4 < 1)
+                {
+                    Console.WriteLine(item.Model);
+                }
+            }
+            
+        }
+        public void PrintFlamable(List<Car> listOfCars)
+        {
+            var filtList = listOfCars.Where(x => x.Cargo.CargoType == "flamable");
+            foreach (var item in filtList.Where(x => x.Engine.EnginePower > 250))
+            {
+                Console.WriteLine(item.Model);
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/RawData/StartUp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            List<Car> listOfCars = new List<Car>();
+            AddCars(rows, listOfCars);
+
+            string command = Console.ReadLine();
+            Car car = new Car();
+
+            if (command == "fragile")
+            {
+                car.PrintFragile(listOfCars);
+            }
+            else if (command == "flamable")
+            {
+                car.PrintFlamable(listOfCars);
+            }
+        }
+
+        private static void AddCars(int rows, List<Car> listOfCars)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                //Input
+                string[] input = Console.ReadLine().Split().ToArray();
+                //Model
+                string model = input[0];
+                //Engine
+                int engineSpeed = int.Parse(input[1]);
+                int enginePower = int.Parse(input[2]);
+                //Cargo
+                int cargoWeight = int.Parse(input[3]);
+                string cargoType = input[4];
+                //Tires 1
+                double tirePressuerOne = double.Parse(input[5]);
+                int tireAgeOne = int.Parse(input[6]);
+                //Tires 2
+                double tirePressuerTwo = double.Parse(input[7]);
+                int tireAgeTwo = int.Parse(input[8]);
+                //Tires 3
+                double tirePressuerThree = double.Parse(input[9]);
+                int tireAgeThree = int.Parse(input[10]);
+                //Tires 4
+                double tirePressuerFour = double.Parse(input[11]);
+                int tireAgeFour = int.Parse(input[12]);
+
+                Engine currEngine = new Engine(engineSpeed, enginePower);
+                Cargo currCargo = new Cargo(cargoWeight, cargoType);
+
+                Tire[] currTires = new Tire[4]
+                {
+                new Tire(tirePressuerOne, tireAgeOne),
+                new Tire(tirePressuerTwo, tireAgeTwo),
+                new Tire(tirePressuerThree, tireAgeThree),
+                new Tire(tirePressuerFour, tireAgeFour),
+                };
+
+                Car currCar = new Car(model, currEngine, currCargo, currTires);
+                listOfCars.Add(currCar);
+            }
+        }
+    }
+}
