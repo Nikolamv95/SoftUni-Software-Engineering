d770b096b1737efaf6b8387d9558e059581d4ad5
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-21 11:20:13 +0200

    C# Advanced tasks

---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/OpinionPoll/Family.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.Members.Add(member);
+        }
+
+        public List<Person> GetMembersOver30()
+        {
+            List<Person> listOfPersons = new List<Person>();
+            listOfPersons = this.Members.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+
+            return listOfPersons;
+        }
+    }
+}
---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/OpinionPoll/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] data = Console.ReadLine().Split().ToArray();
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                Person member = new Person(name, age);
+                family.AddMember(member);
+            }
+
+            List<Person> oldestMember = family.GetMembersOver30();
+
+            foreach (var item in oldestMember)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+        }
+    }
+}
