82a9e7dbe87c4c81358889b9f88c0155b903a3ec
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-25 20:52:52 +0200

    Add tasks to c# Advanced

---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/OldestFamilyMember/Family.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int minValue = int.MinValue;
+            Person oldestPerson = new Person();
+
+            foreach (var item in this.Members)
+            {
+                if (item.Age > minValue)
+                {
+                    minValue = item.Age;
+                    oldestPerson = item;
+                }
+            }
+
+            return oldestPerson;
+        }
+    }
+}
---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] data = Console.ReadLine().Split().ToArray();
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                Person member = new Person(name, age);
+                family.AddMember(member);
+            }
+
+           var oldestMember = family.GetOldestMember();
+           Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
