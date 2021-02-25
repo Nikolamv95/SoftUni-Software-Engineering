fa562bbef99deb964f7c2304cb214d7ddfa16136
Nikolamv95 <nikola.margaritoov@gmail.com>
2021-02-25 20:53:19 +0200

    Add tasks to c# Advanced

---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/DateModifier/DateModifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DateModifier
+{
+    public long CalculateDate(List<DateTime> listOfDates)
+    {
+        TimeSpan result = listOfDates[0] - listOfDates[1];
+        string currTime = result.ToString();
+
+        long finalResult = CalculateExactTime(currTime);
+        return finalResult;
+    }
+
+    private long CalculateExactTime(string currTime)
+    {
+        string[] result = currTime.Split('.').ToArray();
+        long finalResult = Math.Abs(long.Parse(result[0]));
+        return finalResult;
+    }
+}
---
+++ CSharp-Advanced/6.DefiningClasses/2.Excercise/DefineClassPerson/DateModifier/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<DateTime> listOfDateTime = new List<DateTime>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                int year = int.Parse(input[0]);
+                int month = int.Parse(input[1]);
+                int day = int.Parse(input[2]);
+
+                DateTime currTime = new DateTime(year, month, day);
+                listOfDateTime.Add(currTime);
+            }
+
+            DateModifier dateModifier = new DateModifier();
+            long result = dateModifier.CalculateDate(listOfDateTime);
+            Console.WriteLine(result);
+        }
+    }
+}
