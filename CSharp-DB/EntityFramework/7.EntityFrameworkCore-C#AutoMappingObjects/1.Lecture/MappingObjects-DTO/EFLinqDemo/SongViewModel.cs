05235e3085d723000df9ff43eafada09bd121253
Nikola <nikola.margaritoov@gmail.com>
2021-03-10 17:22:59 +0200

    Adding files to EFLinqDemo

--- CSharp-DB/EntityFramework/7.EntityFrameworkCore-C#AutoMappingObjects/1.Lecture/MappingObjects-DTO/EFLinqDemo/SongViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EFLinqDemo
-{
-    public class SongViewModel
-    {
-        public string Name { get; set; }
-        public string SourceName { get; set; }
-        public string Artists { get; set; } = null;
-    }
-}
