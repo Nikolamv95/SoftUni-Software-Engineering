a0664b733d80de47bef329bbfdb723d7be9a258a
Nikola Margaritov <nikola.margaritoov@gmail.com>
2021-04-26 23:20:30 +0300

    SignalR demos

---
+++ CSharp-Asp.NetCore/RealTimeDemos/05. SignalRDemo/wwwroot/site.js
@@ -0,0 +1,50 @@
+﻿// WebSocket = undefined;
+// EventSource = undefined;
+//, signalR.HttpTransportType.LongPolling
+
+let connection = null;
+
+setupConnection = () => {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("/coffeehub")
+        .build();
+
+    // This is the name of the methods ReceiveOrderUpdate. Find this method is CoffeeHub
+    // When the server calls the JS method the code bellow will be 
+    // executed(update) => { document.getElementById("status").innerHTML = update; });
+    connection.on("ReceiveOrderUpdate", (update) => {
+        document.getElementById("status").innerHTML = update;
+    });
+
+    // This is the name of the methods NewOrder. Find this method is CoffeeController
+    connection.on("NewOrder", function (order) {
+        document.getElementById("status").innerHTML = "Someone ordered a " + order.product;
+    });
+
+    // This is the name of the methods Finished. Find this method is CoffeeHub
+    connection.on("Finished", function () {
+        // connection.stop();
+    });
+
+    connection.start()
+        .catch(err => console.error(err.toString()));
+};
+
+setupConnection();
+
+document.getElementById("submit").addEventListener("click", e => {
+    e.preventDefault();
+    const product = document.getElementById("product").value;
+    const size = document.getElementById("size").value;
+
+    fetch("/Coffee",
+        {
+            method: "POST",
+            body: JSON.stringify({ product, size }),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(response => response.text())
+        .then(id => connection.invoke("GetUpdateForOrder", parseInt(id)));
+});
---
+++ CSharp-Asp.NetCore/RealTimeDemos/06. SignalRConsoleClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SignalRConsoleClient
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.SignalR.Client;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Program
+    {
+        public static async Task Main()
+        {
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44305/coffeehub", options => {
+                    var handler = new HttpClientHandler
+                    {
+                        ClientCertificateOptions = ClientCertificateOption.Manual,
+                        ServerCertificateCustomValidationCallback = (httpRequestMessage, cert, cetChain, policyErrors) => true
+                    };
+                    options.HttpMessageHandlerFactory = _ => handler;
+                })
+                .AddMessagePackProtocol()
+                .Build();
+
+            connection.On<Order>(
+                "NewOrder",
+                order => Console.WriteLine($"Somebody ordered {order.Size} {order.Product}"));
+
+            await connection.StartAsync();
+
+            Console.WriteLine("Listening. Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
